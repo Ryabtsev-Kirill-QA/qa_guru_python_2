832e5f9253a199298efa680e1ec663e6677e48e9
Kirill <ryabtsev231297@gmail.com>
2023-09-19 22:34:39 +0300

    add tests

--- test_google_search.py
+++ test_google_search.py
@@ -1,4 +1,3 @@
-import pytest
 from selene.support.shared import browser
 from selene import be, have
 
@@ -6,4 +5,11 @@ from selene import be, have
 def test_search_info_positive():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('oriented Web UI browser test in Python'))
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_search_info_negative():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('fdhfhgnthrjtjhnt2134325').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
+    print('Поиск не выдает результатов')
